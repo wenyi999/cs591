c874a510c3afccd0d1dcd82175b7ab10cf90d17b
wenyi999 <2947711395@qq.com>
2020-11-13 22:43:47 +0800

    task 2 finished

--- skeleton/assignment3.py
+++ skeleton/assignment3.py
@@ -1,6 +1,7 @@
 from PythonApplication1 import Scan,Distinct,Map
 from random import randint
 import logging
+from sklearn.model_selection import train_test_split
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
@@ -26,12 +27,23 @@ if __name__ == "__main__":
             keys_of_one_attr=i.keys()
             for j in keys_of_one_attr:
                 if j=="empty":
-                    keys[j]="0"
+                    keys[j]=0
                 else:
-                    keys[j]=str(index)
+                    keys[j]=index
                     index+=1
         scan_data=Scan('../data/sample.csv',None,False,False)
         map_data=Map(scan_data,keys)
         data_ETL=map_data.get_next()
-        logger.info("An example of the data after the ETL:")
         logger.info(data_ETL[0])
+        x=[]
+        y=[]
+        for i in data_ETL:
+            for j in range(0,len(i)):
+                i[j]=int(i[j])
+            x.append(i[0:len(i)-1])
+            y.append(i[len(i)-1])
+        x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.30, random_state=42)
+        #logger.info(x_train[0])
+
+
